ae4fc901d94330f1d88bd094b8fcaf8cc5c9921e
Aryabhatt <bhattsahab314@gmail.com>
2023-09-24 13:30:26 +0530

    Defined CarManager class. Created a car at a random location on y-axis with random colour. Defined the move() method for it

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,28 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+        self.create_car()
+
+    def create_car(self):
+        x_pos = 300
+        y_pos = random.randint(-250, 250)
+        new_car = Turtle()
+        new_car.shape("square")
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.color(random.choice(COLORS))
+        new_car.penup()
+        new_car.goto(x_pos, y_pos)
+        self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
