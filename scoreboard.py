2512f16ed4382b767d7ccffa9d0c01181d06cfc6
Aryabhatt <bhattsahab314@gmail.com>
2023-09-24 12:51:54 +0530

    Defined Scoreboard class with constructor, update_level() and write_level() methods

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,21 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.goto(-280, 260)
+        self.level = 1
+        self.hideturtle()
+        self.write_level()
+
+    def update_level(self):
+        self.level += 1
+        self.clear()
+        self.write_level()
+
+    def write_level(self):
+        self.write(f"Level : {self.level}", align="left", font=FONT)
