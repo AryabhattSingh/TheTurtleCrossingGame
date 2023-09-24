ac32249301aa9b1a54645a1dcd9694161bc6d09a
Aryabhatt <bhattsahab314@gmail.com>
2023-09-24 13:35:28 +0530

    Slowed the production of cars using an if condition in CarManager.create_car()

--- car_manager.py
+++ car_manager.py
@@ -9,19 +9,22 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
+        self.hideturtle()
         self.all_cars = []
         self.create_car()
 
     def create_car(self):
-        x_pos = 300
-        y_pos = random.randint(-250, 250)
-        new_car = Turtle()
-        new_car.shape("square")
-        new_car.shapesize(stretch_wid=1, stretch_len=2)
-        new_car.color(random.choice(COLORS))
-        new_car.penup()
-        new_car.goto(x_pos, y_pos)
-        self.all_cars.append(new_car)
+        random_choice = random.randint(1,6)
+        if random_choice == 1:
+            x_pos = 300
+            y_pos = random.randint(-250, 250)
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.goto(x_pos, y_pos)
+            self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
