88afbdd96a877a18203b6cd2fc90312d4deb64be
Aryabhatt <bhattsahab314@gmail.com>
2023-09-24 13:00:47 +0530

    Defined the initial position of the player and the move method for it

--- player.py
+++ player.py
@@ -1,7 +1,17 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(x=0, y=-280)
+        self.left(90)
+
+    def move(self):
+        self.forward(20)
