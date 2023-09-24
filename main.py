845ca9c0355f9ffdaf3355bc1156c9dc5d64c199
Aryabhatt <bhattsahab314@gmail.com>
2023-09-24 12:53:05 +0530

    Imported classes and created objects of Player, CarManager and Scoreboard. Added game loop

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import time
 from turtle import Screen
 
+from car_manager import CarManager
+from player import Player
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+car = CarManager()
+level = Scoreboard()
+
+# Game loop
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
 
 screen.exitonclick()
